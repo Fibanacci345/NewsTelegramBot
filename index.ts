206bf597c6e997d7cdea25d78ea9d14874d8c44d
Alexander Novitskiy <alexandernovitsky77@gmail.com>
2023-03-30 17:23:57 +0300

    Link database to ORM and createrd forlders #20 #21

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-import config from "./config.js";
+import config from "./config";
+import { connect } from "mongoose";
 import { Bot } from "grammy";
 
 const bot = new Bot(config.botToken);
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 bot.on("message", (ctx) => ctx.reply("Nice"));
+
+connect(config.databaseConnectionString);
 
 bot.start();
