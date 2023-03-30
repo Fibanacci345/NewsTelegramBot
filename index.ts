9f92f90ed76c8dfc18c0ffc1cd90df4387114f2d
GlebHal <hammanov.gleb@gmail.com>
2023-03-30 13:59:20 +0300

    Added simple bot logic

---
+++ index.ts
@@ -0,0 +1,10 @@
+import config from "./config.js";
+import { Bot } from "grammy";
+
+
+const bot = new Bot(config.botToken);
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.on("message", (ctx) => ctx.reply("Nice"));
+
+bot.start();
