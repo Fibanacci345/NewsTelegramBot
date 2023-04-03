2f737dd86a8b55706be939700b9feef33550b229
GlebHal <hammanov.gleb@gmail.com>
2023-04-03 12:19:12 +0300

    User handlers small changes

--- handlers/users.ts
+++ handlers/users.ts
@@ -5,32 +5,50 @@ import { parseMode } from "../bot";
 import { Context } from "grammy";
 
 export const newsHandler = async (ctx: Context): Promise<void> => {
-    const headlines = await News.getTopHeadlinesFormatted({
-        country: "us",
-    });
+    try {
+        const headlines: string[] = await News.getTopHeadlinesFormatted({
+            country: "us",
+        });
 
-    ctx.reply(headlines[0], {
-        parse_mode: parseMode,
-        reply_markup: getNewsNavMenu(headlines.length - 1, 1),
-    });
+        if (headlines.length === 0) throw new Error("There is no headlines");
+
+        ctx.reply(headlines[0], {
+            parse_mode: parseMode,
+            reply_markup: getNewsNavMenu(headlines.length - 1, 1),
+        });
+    } catch (error) {
+        ctx.reply("Oops! Something went wrong.");
+
+        console.log(error);
+    }
 }
 
 export const newsCallbackHandler = async (ctx: Context): Promise<void> => {
-    const headlines = await News.getTopHeadlinesFormatted({
-        country: "us",
-    });
+    try {
+        const headlines: string[] = await News.getTopHeadlinesFormatted({
+            country: "us",
+        });
 
-    if (headlines.length === 0) ctx.reply("Oops!")
+        if (headlines.length === 0) throw new Error("There is no headlines");
 
-    const currentIndex = parseInt(ctx.callbackQuery?.data?.split(" ")[1]!);
-    let nextIndex = currentIndex + 1;
-    let prevIndex = currentIndex - 1;
+        if (typeof ctx.callbackQuery === "undefined" || typeof ctx.callbackQuery.data === "undefined") {
+            throw new Error("Can't get current index");
+        }
 
-    if (nextIndex >= headlines.length) nextIndex = 0;
-    if (prevIndex <= -1) prevIndex = headlines.length - 1;
+        const currentIndex: number = parseInt(ctx.callbackQuery.data.split(" ")[1]);
+        let nextIndex: number = currentIndex + 1;
+        let prevIndex: number = currentIndex - 1;
 
-    ctx.reply(headlines[currentIndex], {
-        parse_mode: parseMode,
-        reply_markup: getNewsNavMenu(prevIndex, nextIndex)
-    });
+        if (nextIndex >= headlines.length) nextIndex = 0;
+        if (prevIndex <= -1) prevIndex = headlines.length - 1;
+
+        ctx.reply(headlines[currentIndex], {
+            parse_mode: parseMode,
+            reply_markup: getNewsNavMenu(prevIndex, nextIndex)
+        });
+    } catch (error) {
+        ctx.reply("Oops! Something went wrong.");
+
+        console.log(error);
+    }
 }
