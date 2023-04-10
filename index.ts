10e814d124484bf6e0cd15e8f726f1168d5b685c
GlebHal <hammanov.gleb@gmail.com>
2023-04-10 16:01:43 +0300

    Small changes

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import config from "./config";
 import { connect } from "mongoose";
-import { bot, commands } from "./bot";
+import { Bot } from "grammy";
+
+const bot = new Bot(config.botToken);
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+bot.on("message", (ctx) => ctx.reply("Nice"));
 
 connect(config.databaseConnectionString);
-
-bot.start().then(() => {
-    bot.api.setMyCommands(commands)
-});
