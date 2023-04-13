f2c292f5d6ae7f584683ca23f5be5cfaf9453aa5
GlebHal <hammanov.gleb@gmail.com>
2023-04-13 15:16:26 +0300

    Added common folder + small fixes

--- handlers/user.ts
+++ handlers/user.ts
@@ -1,69 +1,11 @@
 import News from "../utils/news";
-import { getNewsNav, getHeadlinesNav, movingDirection } from "../keyboards/inline";
+import { getNewsNav, getHeadlinesNav } from "../keyboards/inline";
 import { parseMode } from "../bot";
 
 import { Context } from "grammy";
 import { currentState, setState } from "../state";
 
-enum CallbackParamsIndexes {
-    commandTitle = 0,
-    currentIndex,
-    direction,
-    query
-}
-
-interface IMovingButtonCallback {
-    current: number,
-    direction: movingDirection
-}
-
-interface INewsMovingButtonCallback extends IMovingButtonCallback {
-    query: string,
-}
-
-const parseMovingButtonCallback = (callback: string): IMovingButtonCallback => {
-    let params: string[]
-    let current: number;
-    let direction: movingDirection;
-
-    try {
-        params = callback.split(" ");
-        current = parseInt(params[CallbackParamsIndexes.currentIndex]);
-        direction = params[CallbackParamsIndexes.direction] as movingDirection;
-    } catch (err) {
-        throw new Error("Can't parse callback");
-    }
-
-    return {
-        current: current,
-        direction: direction
-    }
-}
-
-const parseNewsMovingButtonCallback = (callback: string): INewsMovingButtonCallback => {
-    const baseParams: IMovingButtonCallback = parseMovingButtonCallback(callback);
-    let query;
-
-    try {
-        query = callback.split(" ")[CallbackParamsIndexes.query];
-    } catch (err) {
-        throw new Error("Can't parse query from callback");
-    }
-
-    return {
-        ...baseParams,
-        query,
-    };
-}
-
-const getValidatedIndex = (currentIndex: number, currentDirection: movingDirection, maxSize: number): number => {
-    currentDirection == 'f' ? currentIndex++ : currentIndex--;
-
-    if (currentIndex >= maxSize) currentIndex = 0;
-    else if (currentIndex == -1) currentIndex = maxSize - 1;
-
-    return currentIndex;
-}
+import helpers, { CallbackParamsIndexes, IMovingButtonCallback, INewsMovingButtonCallback } from "../common/handlerHelpers";
 
 const newsHandler = (ctx: Context) => {
     ctx.reply("Hello! Send me a topic.");
@@ -108,8 +50,7 @@ const newsCallbackHandler = async (ctx: Context): Promise<void> => {
         if (typeof ctx.callbackQuery === "undefined") throw new Error("There is no callbackQuery in newsCallbackHandler");
         if (typeof ctx.callbackQuery.data === "undefined") throw new Error("There is no callbackQuery data in newsCallbackHandler");
 
-        const movingButtonCallback: INewsMovingButtonCallback = parseNewsMovingButtonCallback(ctx.callbackQuery.data);
-
+        const movingButtonCallback: INewsMovingButtonCallback = helpers.parseNewsMovingButtonCallback(ctx.callbackQuery.data);
         const articles: string[] = await News.getEverythingFormatted({ q: movingButtonCallback.query })
 
         if (articles.length === 0) {
@@ -118,7 +59,7 @@ const newsCallbackHandler = async (ctx: Context): Promise<void> => {
             return;
         }
 
-        let movingIndex = getValidatedIndex(movingButtonCallback.current, movingButtonCallback.direction, articles.length);
+        let movingIndex = helpers.getValidatedIndex(movingButtonCallback.current, movingButtonCallback.direction, articles.length);
 
         ctx.reply(articles[movingIndex], {
             parse_mode: parseMode,
@@ -129,7 +70,6 @@ const newsCallbackHandler = async (ctx: Context): Promise<void> => {
 
         console.log(err);
     }
-
 }
 
 const headlinesHandler = async (ctx: Context): Promise<void> => {
@@ -158,7 +98,7 @@ const headlinesHandler = async (ctx: Context): Promise<void> => {
 }
 
 const headlinesCallbackHandler = async (ctx: Context): Promise<void> => {
-    const movingButtonCallback = parseMovingButtonCallback(ctx.callbackQuery?.data!);
+    const movingButtonCallback = helpers.parseMovingButtonCallback(ctx.callbackQuery?.data!);
 
     const headlines: string[] = await News.getTopHeadlinesFormatted({
         country: "us",
@@ -170,7 +110,7 @@ const headlinesCallbackHandler = async (ctx: Context): Promise<void> => {
         return;
     }
 
-    let movingIndex = getValidatedIndex(movingButtonCallback.current, movingButtonCallback.direction, headlines.length);
+    let movingIndex = helpers.getValidatedIndex(movingButtonCallback.current, movingButtonCallback.direction, headlines.length);
 
     ctx.reply(headlines[movingIndex], {
         parse_mode: parseMode,
